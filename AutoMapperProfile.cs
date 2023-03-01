18d915e54e18cac5709a7a9f6311f65573507dc1
Ademola <sherggs@gmail.com>
2023-03-01 23:48:02 +0000

    AutoMapper Profile

---
+++ AutoMapperProfile.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace Net7
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Character, GetCharacterDtos>();
+            CreateMap<AddCharacterDtos, Character>();
+        }
+    }
+}
