15cece239c3cda945400a71d98e0e096842ce1ef
Ademola <sherggs@gmail.com>
2023-03-01 19:59:13 +0000

    add autoMapper

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 global using Net7.Models;
 global using Net7.Services.CharacterService;
+global using Net7.Dtos.Character;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ICharacterService, CharacterService>();
+builder.Services.AddAutoMapper(typeof(Program).Assembly); // Add AutoMapper
+builder.Services.AddScoped<ICharacterService, CharacterService>(); // Add CharacterService
 
 var app = builder.Build();
 
