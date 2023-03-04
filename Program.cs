2bbbed9b08b49997737d10d933a5d150a15d2b48
Ademola <sherggs@gmail.com>
2023-03-04 16:08:45 +0000

    DbContext and Connection String Config

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 global using Net7.Models;
+global using static System.Console;
 global using Net7.Services.CharacterService;
 global using Net7.Dtos.Character;
 global using AutoMapper;
-
+global using Microsoft.EntityFrameworkCore;
+global using Net7.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
