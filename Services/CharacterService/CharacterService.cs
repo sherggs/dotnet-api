c2d74df83d0e4da45a05a11c01999e05246477c0
Ademola <sherggs@gmail.com>
2023-03-04 16:13:14 +0000

    DBCharacter GETALL

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
-using static System.Console;
+
 
 namespace Net7.Services.CharacterService
 {
@@ -11,14 +11,30 @@ namespace Net7.Services.CharacterService
     {  
          private static List<Character> characters = new List<Character> {
             new Character(),
-            new Character { Id = 1, Name = "Mason", Class = RpgClass.Knight, HitPoints = 70}
+            new Character { Id = 1, Name = "Mason", Class = RpgClass.Knight, HitPoints = 70},
+            new Character { Id = 2, Name = "Ademola", Class = RpgClass.Mage, HitPoints = 100}
         };
         private readonly IMapper _mapper;
-        public CharacterService(IMapper mapper)
+        private readonly DataContext _context;
+        public CharacterService(IMapper mapper, DataContext context)
         {
+            _context = context;
             _mapper = mapper;
         }
-
+        public async Task<ServiceResponse<GetCharacterDtos>> GetCharacterById(int id) // Get a character by id
+        {
+            var serviceResponse = new ServiceResponse<GetCharacterDtos>();
+            var character = characters.FirstOrDefault(c => c.Id == id); // Get the character from the list
+            serviceResponse.Data = _mapper.Map<GetCharacterDtos>(character);
+            return serviceResponse;
+        } 
+            public async Task<ServiceResponse<List<GetCharacterDtos>>> GetAllCharacters()
+        {
+            var serviceResponse = new ServiceResponse<List<GetCharacterDtos>>(); // Create a new service response
+            var dbCharacters = await _context.Characters.ToListAsync(); // Get all characters from the database
+            serviceResponse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDtos>(c)).ToList(); // Map the characters to a list of GetCharacterDtos
+            return serviceResponse; 
+        }
         public async Task<ServiceResponse<List<GetCharacterDtos>>> AddCharacter(AddCharacterDtos newCharacter)
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDtos>>();
@@ -30,38 +46,6 @@ namespace Net7.Services.CharacterService
             return serviceResponse;
            
         }
-
-        public async Task<ServiceResponse<List<GetCharacterDtos>>> DeleteCharacter(int id)
-        {
-            var serviceResponse = new ServiceResponse<List<GetCharacterDtos>>();
-            try
-            {
-                Character character = characters.First(c => c.Id == id);
-                characters.Remove(character);
-                serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDtos>(c)).ToList();
-            }
-            catch (Exception ex)
-            {
-                serviceResponse.Success = false;
-                serviceResponse.Message = ex.Message;
-            }
-            return serviceResponse;
-        }
-
-        public async Task<ServiceResponse<List<GetCharacterDtos>>> GetAllCharacters()
-        {
-            var serviceResponse = new ServiceResponse<List<GetCharacterDtos>>();
-            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDtos>(c)).ToList();
-            return serviceResponse; 
-        }
-
-        public async Task<ServiceResponse<GetCharacterDtos>> GetCharacterById(int id)
-        {
-            var serviceResponse = new ServiceResponse<GetCharacterDtos>();
-            var character = characters.FirstOrDefault(c => c.Id == id);
-            serviceResponse.Data = _mapper.Map<GetCharacterDtos>(character);
-            return serviceResponse;
-        } 
         public async Task<ServiceResponse<GetCharacterDtos>> UpdateCharacter(UpdateCharacterDtos updatedCharacter)
         {
             var serviceResponse = new ServiceResponse<GetCharacterDtos>();
@@ -88,6 +72,22 @@ namespace Net7.Services.CharacterService
             }
             return serviceResponse;
         }
-    
+
+        public async Task<ServiceResponse<List<GetCharacterDtos>>> DeleteCharacter(int id)
+        {
+            var serviceResponse = new ServiceResponse<List<GetCharacterDtos>>();
+            try
+            {
+                Character character = characters.First(c => c.Id == id);
+                characters.Remove(character);
+                serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDtos>(c)).ToList();
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = ex.Message;
+            }
+            return serviceResponse;
+        }
     }
 }
