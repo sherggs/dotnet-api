f09b227e21bec375ffc1886b23ea28080f03e3a4
Ademola <sherggs@gmail.com>
2023-03-25 02:45:18 +0000

    Weapons Model

---
+++ Models/Weapon.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Net7.Models
+{
+    public class Weapon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public int Damage { get; set; } 
+        public Character? Character { get; set; }
+
+
+    }
+}
