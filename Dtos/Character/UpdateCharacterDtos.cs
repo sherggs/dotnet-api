dad4e84bdb3966bcee3d70b57cf4be918f1b6577
Ademola <sherggs@gmail.com>
2023-03-25 02:45:01 +0000

    Name Update

--- Dtos/Character/UpdateCharacterDtos.cs
+++ Dtos/Character/UpdateCharacterDtos.cs
@@ -13,6 +13,6 @@ namespace Net7.Dtos.Character
         public int Strength { get; set; } = 10;
         public int Defense { get; set; } = 10;
         public int Intelligence { get; set; } = 10;
-        public RpgClass Class { get; set; } = RpgClass.Cleric;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
     }
 }
--- Models/Character.cs
+++ Models/Character.cs
@@ -8,14 +8,16 @@ namespace Net7.Models
     public class Character
     {
         public int Id { get; set; }
-        public string Name { get; set; } = "Ghost";
+        public string Name { get; set; } = "Ademola";
         public int HitPoints { get; set; } = 100;
         public int Strength { get; set; } = 10;
         public int Defense { get; set; } = 10;
         public int Intelligence { get; set; } = 10;
-        public RpgClass Class { get; set; } = RpgClass.Cleric;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
 
         public User? User { get; set; }
+
+        public Weapon? Weapon { get; set; }
     }
 }
 
