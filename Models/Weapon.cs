5c5525b76e7f74052d7313e047c12c34e7c5fa68
Ademola <sherggs@gmail.com>
2023-04-02 19:01:21 +0100

    Add Weapons

--- Models/Weapon.cs
+++ Models/Weapon.cs
@@ -10,6 +10,7 @@ namespace Net7.Models
         public int Id { get; set; }
         public string Name { get; set; } = String.Empty;
         public int Damage { get; set; } 
+        public int CharacterId { get; set; }
         public Character? Character { get; set; }
 
 
